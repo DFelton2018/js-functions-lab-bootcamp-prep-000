cd370418962a0643016f2866680b2246a42f6cd1
Duane Felton <dfelton2010@gmail.com>
2018-06-09 20:00:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,8 @@ function happyHolidayTo(holiday, name) {
 function holidayCountdown(holiday, days) {
   return "It's 20 days until Mother's Day!";
 }
+
+function sayHelloTo() {
+  var title = "Lord";
+  var name = "Baelish";
+}
